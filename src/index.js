62c88b36779103f17c7a64ea2dfe2f946ec500c4
cebolapipoca <alexpiedade20@gmail.com>
2025-05-22 02:48:40 -0300

    site quase completo

--- src/index.js
+++ src/index.js
@@ -53,12 +53,11 @@ const Router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
                         root.render(
                                 <Contextozinho>
-                                        <App/>
+                                <RouterProvider router={Router}/>
                                 </Contextozinho> 
                         );
 
 
-//<RouterProvider router={Router}/>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
